af9ad317185e65ae631183481c3bd68b20af9e73
DELL <DELL@DESKTOP-A5KQQNL>
2023-03-21 21:35:36 +0530

    Added

---
+++ Training1/Test1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Training1;
+
+/**
+ * @author DELL
+ *
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
