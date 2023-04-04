9d2b7772f0cc03ac5c621ab566122a8b357e48b1
DELL <DELL@DESKTOP-A5KQQNL>
2023-04-04 19:44:26 +0530

    Program Commit

---
+++ src/TrainingCodes/Program5.java
@@ -0,0 +1,17 @@
+package TrainingCodes;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		
+				int i;
+				for(i=1;i<=5;i++){
+
+					System.out.println("*@");
+					
+				}
+				
+			}
+       }
+
+
