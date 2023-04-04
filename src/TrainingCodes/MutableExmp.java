6baa7d726afe408ea4b0f1a729b1628d46ba294a
DELL <DELL@DESKTOP-A5KQQNL>
2023-04-04 21:35:26 +0530

    Program Commit

---
+++ src/TrainingCodes/MutableExmp.java
@@ -0,0 +1,16 @@
+package TrainingCodes;
+public class MutableExmp {
+public static void main(String[] args) {
+		        String s1="Ravana";
+				String s2="Sita";
+				 s1=s1.concat(s2);
+				 System.out.println(s1);
+				
+				
+				// TODO Auto-generated method stub
+
+			}
+
+		
+
+}
