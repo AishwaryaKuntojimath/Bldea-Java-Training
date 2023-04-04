b7a5f04900e8264f6c625d92af010aabb8984737
DELL <DELL@DESKTOP-A5KQQNL>
2023-04-04 21:37:59 +0530

    Program Commit

---
+++ src/TrainingCodes/ImmutableExmp.java
@@ -0,0 +1,13 @@
+package TrainingCodes;
+
+public class ImmutableExmp {
+
+	public static void main(String[] args) {
+		
+				StringBuffer sb1=new StringBuffer("Rama");
+				StringBuffer sb2=new StringBuffer("Sita");
+				sb1.append(sb2);
+				System.out.println(sb1);
+				
+			}
+          }
