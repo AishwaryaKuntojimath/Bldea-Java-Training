356fd1b6486ae3671b298536c7ec42c8d4e9c201
DELL <DELL@DESKTOP-A5KQQNL>
2023-04-04 19:39:35 +0530

    Program commit

---
+++ src/TrainingCodes/Program3.java
@@ -0,0 +1,19 @@
+package TrainingCodes;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		
+				int i=1;
+				do{
+
+					System.out.println("Hello World!.." );
+					i++;
+				}while(i<=5);
+				// TODO Auto-generated method stub
+
+			}
+}
+
+		
+		
