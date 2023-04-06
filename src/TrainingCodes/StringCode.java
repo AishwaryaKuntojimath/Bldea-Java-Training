6bb46a90b35a4ba9b00f7a503104c8edcce7b3ac
DELL <DELL@DESKTOP-A5KQQNL>
2023-04-06 20:38:45 +0530

    Program Commit

---
+++ src/TrainingCodes/StringCode.java
@@ -0,0 +1,25 @@
+package TrainingCodes;
+
+public class StringCode {
+
+	public static void main(String[] args) {
+		            String s1="BAMA";
+					String s2="RAMA";
+					if(s1==s2) {
+						System.out.println("Address are equal");
+					}
+					else {
+						System.out.println("Adderess are not equal");
+						
+						}
+					}
+
+					
+				}
+				
+			
+
+				
+		
+
+
