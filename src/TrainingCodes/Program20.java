1138e91ab14d22f3f5722f034d80a9232b1e7095
DELL <DELL@DESKTOP-A5KQQNL>
2023-04-06 23:00:39 +0530

    Program Commit

---
+++ src/TrainingCodes/Program20.java
@@ -0,0 +1,17 @@
+package TrainingCodes;
+import java.util.Scanner;
+
+public class Program20 {
+
+	public static void main(String[] args) {
+						
+				for (int i=1;i<=5;i++) {
+					
+					System.out.println("Hello World!.." );
+				}
+
+
+			}
+         }
+
+	
