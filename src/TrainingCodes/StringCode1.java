7d6be705b7adbdf1ecdadad09f563cf96223959b
DELL <DELL@DESKTOP-A5KQQNL>
2023-04-06 20:43:27 +0530

    Program Commit

---
+++ src/TrainingCodes/StringCode1.java
@@ -0,0 +1,36 @@
+package TrainingCodes;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class StringCode1 {
+
+	public static void main(String[] args) {
+	            //step-1;
+				Scanner sc=new Scanner(System.in);
+				System.out.println("string-1:");
+				String s1=sc.next();
+				System.out.println("string-2");
+				String s2=sc.next();
+				
+				//step-2
+				char[] c1=s1.toCharArray();
+				char[] c2=s2.toCharArray();
+				
+				//step-3
+				
+				Arrays.sort(c1);
+				Arrays.sort(c2);
+				
+				//step-4
+				if(Arrays.equals (c1,c2)) {
+					System.out.println("Anagrams");
+				}
+				else {
+					System.out.println("Not Anagramms");
+					
+				}
+			
+	}
+
+}
